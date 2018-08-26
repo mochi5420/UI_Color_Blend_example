46d4ee773bc892d95df4a9f2f60580dd319e6c5b
mochi5420 <k.mochida5420@gmail.com>
2018-08-27 00:50:33 +0900

    コメントを追加

--- Assets/Scripts/CustomImage.cs
+++ Assets/Scripts/CustomImage.cs
@@ -16,6 +16,7 @@ public class CustomImage : Image
     {
         get
         {
+            // 加算の場合はアルファ値を0.0 ~ 0.5, 乗算の場合は0.5 ~ 1.0になるように変換をかける
             return new Color(base.color.r, base.color.g, base.color.b, (base.color.a + (int)_blendType) / 2.0f);
         }
         set { base.color = value; }
