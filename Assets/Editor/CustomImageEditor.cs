62fab54cb2dd0d9fc977d4a89af4f26a4f10a049
mochi5420 <k.mochida5420@gmail.com>
2018-08-26 22:48:27 +0900

    完成した最初のバージョン

---
+++ Assets/Editor/CustomImageEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(CustomImage), true)]
+[CanEditMultipleObjects]
+public class CostomImageEditor : ImageEditor
+{
+
+    SerializedProperty _colorType;
+
+    protected override void OnEnable()
+    {
+        _colorType = serializedObject.FindProperty("_colorType");
+        base.OnEnable();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        _colorType.intValue = (int)(CustomImage.TYPE)EditorGUILayout.EnumPopup("Type", (CustomImage.TYPE)_colorType.intValue);
+
+        serializedObject.ApplyModifiedProperties();
+
+        base.OnInspectorGUI();
+    }
+}
+
---
+++ Assets/Scripts/CustomImage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CustomImage : Image
+{
+    public enum TYPE
+    {
+        ADDITIVE = 0,
+        MULTIPLE = 1,
+    }
+
+    [SerializeField] private TYPE _colorType = TYPE.MULTIPLE;
+
+    public override Color color
+    {
+        get
+        {
+            return new Color(base.color.r, base.color.g, base.color.b, (base.color.a + (int)_colorType) / 2.0f);
+        }
+        set { base.color = value; }
+    }
+}
